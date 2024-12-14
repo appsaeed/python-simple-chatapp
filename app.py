63ed4db28ef82c759850b2d684afc9f0653a40b1
Saeed <78552486+appsaeed@users.noreply.github.com>
2024-12-15 00:07:56 +0600

    Display running server url on the console

--- app.py
+++ app.py
@@ -55,4 +55,11 @@ def handle_update_username(data):
     }, broadcast=True)
 
 if __name__ == "__main__":
-    socketio.run(app) 
+    host = "0.0.0.0"
+    port = 8080
+
+    # Log the server's URL
+    print(f"App running at: http://{'localhost' if host == '0.0.0.0' else host}:{port}")
+
+    # Start the Socket.IO server
+    socketio.run(app, host=host, port=port)
